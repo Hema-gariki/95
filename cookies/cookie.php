819be2e70c46347a94e511e70d95f660a6cb441d
Hema Gariki <hema@gmail.com>
2026-02-18 14:41:26 -0600

    Updated cookie functionality

--- cookies/cookie.php
+++ cookies/cookie.php
@@ -1,40 +1,42 @@
 <?php
-  if (isset($_GET['action'])) {
+$cookie_name = "user";
+
+if (isset($_GET['action'])) {
     $action = $_GET['action'];
-    // Handle cookie update
-    $cookie_name = "user";
-     if ($action == "update") {
+
+    // Update cookie
+    if ($action == "update" && isset($_GET['value']) && $_GET['value'] != "") {
         $user = $_GET['value'];
-        setcookie($cookie_name, $user , time() + 300, "/");
+        setcookie($cookie_name, $user, time() + 300, "/");
     }
 
-    // Handle cookie removal
+    // Remove cookie
     if ($action == "remove") {
         setcookie($cookie_name, "", time() - 300, "/");
     }
 
-    // Reload Page
+    // Reload page
     header("Location: cookie.php");
     exit();
-    }
+}
 ?>
 
 <html>
-  <head>
-    <link rel="stylesheet" href="custom_style.css">
-  </head>
-  <body>
-  <div id="cookie_div">
-  <?php
-    $cookie_name = "user";
-    if(!isset($_COOKIE[$cookie_name])) {
-      echo "Cookie is not set for this site!";
-    } else {
-      echo "Cookie '" . $cookie_name . "' is set!<br>";
-      echo "Value is: " . $_COOKIE[$cookie_name];
-    }
-    include "cookie_input.html";
-  ?>
-  </div>
-  </body>
+<head>
+  <link rel="stylesheet" href="custom_style.css">
+</head>
+<body>
+<div id="cookie_div">
+<?php
+if (!isset($_COOKIE[$cookie_name])) {
+    echo "Cookie is not set for this site!";
+} else {
+    echo "Cookie '" . $cookie_name . "' is set!<br>";
+    echo "Value is: " . $_COOKIE[$cookie_name];
+}
+
+include "cookie_index.html";
+?>
+</div>
+</body>
 </html>
